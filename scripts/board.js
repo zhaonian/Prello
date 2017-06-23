a65692f475edb78867d40676022ae77ed8d287b1
Zhaonian Luan <zhaonial@uci.edu>
2017-06-22 18:16:56 -0700

    added basic skeleton using pure html css js

---
+++ scripts/board.js
@@ -0,0 +1,7 @@
+function showMenu() {
+    document.getElementById("drop-down").classList.add("show-menu");
+}
+
+function closeMenu() {
+        document.getElementById("drop-down").classList.remove("show-menu");
+}
